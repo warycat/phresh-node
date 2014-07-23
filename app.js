8264edd9136db002e1b30eba94b65ef96a8e76d0
Larry Fantasy <larry.fantasy@gmail.com>
2014-07-23 15:26:40 -0700

    rm content type

--- app.js
+++ app.js
@@ -40,9 +40,9 @@ app.configure('development', function(){
 });
 
 app.all('*', function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*/*');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  // res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 
